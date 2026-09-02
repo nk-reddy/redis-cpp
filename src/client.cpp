f9a32cced507e04254887413303f37fd08612223
Nikhil Reddy <nikhilreddy6179@gmail.com>
2026-09-02 13:44:01 -0400

    codecrafters submit [skip ci]

--- src/client.cpp
+++ src/client.cpp
@@ -48,7 +48,7 @@ void handle_client(int client_fd, Store &store) {
                     while (!queuedCommands.empty()) {
                         std::vector<std::string> currentCommand = queuedCommands.front();
                         std::string currentResponse = handle_command(currentCommand[0], currentCommand, store);
-                        response += "+" + currentResponse + "\r\n";
+                        response += currentResponse;
                         queuedCommands.pop();
                     }
                 }
