34bf68cf227b44699af974a336be53361c2d507f
Nikhil Reddy <nikhilreddy6179@gmail.com>
2026-09-02 15:56:57 -0400

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -35,7 +35,13 @@ int main(int argc, char **argv) {
   struct sockaddr_in server_addr;
   server_addr.sin_family = AF_INET;
   server_addr.sin_addr.s_addr = INADDR_ANY;
-  server_addr.sin_port = htons(6379);
+
+  // add custom port configurability
+  int port = 6379;
+  if (argc == 3 && std::string(argv[1]) == "--port") {
+    port = std::stoi(argv[2]);
+  }
+  server_addr.sin_port = htons(port);
 
   if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) != 0) {
     std::cerr << "Failed to bind to port 6379\n";
