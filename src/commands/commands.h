df18cf125dd04ae561d50de2dca3e09b22d406a2
Nikhil Reddy <nikhilreddy6179@gmail.com>
2026-09-04 19:22:06 -0400

    codecrafters submit [skip ci]

--- src/commands/commands.h
+++ src/commands/commands.h
@@ -7,6 +7,28 @@
 #include <vector> 
 
 std::string handle_command(const std::string &command, const std::vector<std::string> &data, Store &store, ServerState &server, const std::string &rawCommand = "");
+std::string handle_command(const std::string &command, const std::vector<std::string> &data, Store &store, ServerState &server);
+std::string handle_command_default();
+std::string handle_command_echo(const std::vector<std::string>& args);
+std::string handle_command_set(const std::vector<std::string>& args, Store &store);
+std::string handle_command_get(const std::vector<std::string>& args, Store &store);
+std::string handle_command_rpush(const std::vector<std::string>& args, Store &store);
+std::string handle_command_lrange(const std::vector<std::string>& args, Store &store);
+std::string handle_command_lpush(const std::vector<std::string>& args, Store &store);
+std::string handle_command_llen(const std::vector<std::string>& args, Store &store);
+std::string handle_command_lpop(const std::vector<std::string>& args, Store &store);
+std::string handle_command_blpop(const std::vector<std::string>& args, Store &store);
+std::string handle_command_type(const std::vector<std::string>& args, Store &store);
+std::string handle_command_xadd(const std::vector<std::string>& args, Store &store);
+std::string handle_command_xrange(const std::vector<std::string>& args, Store &store);
+std::string handle_command_xread(const std::vector<std::string>& args, Store &store);
+std::string handle_command_incr(const std::vector<std::string>& args, Store &store);
+std::string handle_command_info(const std::vector<std::string>& args, ServerState &server);
+std::string handle_command_replconf();
 
 // special commands
 void handle_command_psync(int client_fd, const std::vector<std::string>& args, ServerState &server);
+
+// others
+bool modifying_command(const std::string &command);
+void propogate_command_to_replicas(const std::string &rawCommand, ServerState &server);
