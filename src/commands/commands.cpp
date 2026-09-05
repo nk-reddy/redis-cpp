3cf966f1e2791545ccaf56bfbedf7390581f4e23
Nikhil Reddy <nikhilreddy6179@gmail.com>
2026-09-05 14:23:45 -0400

    codecrafters submit [skip ci]

--- src/commands/commands.cpp
+++ src/commands/commands.cpp
@@ -65,6 +65,9 @@ std::string handle_command(const std::string &command, const std::vector<std::st
     else if (command == "wait") {
         response = handle_command_wait(data, server);
     }
+    else if (command == "config") {
+        response = handle_command_config(data, server);
+    }
     else {
         response = handle_command_default();
     }
