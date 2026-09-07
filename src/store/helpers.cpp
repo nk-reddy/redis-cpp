3a7f73d9ef8fddf783126fe17d8b691351bb4120
Nikhil Reddy <nikhilreddy6179@gmail.com>
2026-09-07 00:34:40 -0400

    codecrafters submit [skip ci]

--- src/store/helpers.cpp
+++ src/store/helpers.cpp
@@ -474,7 +474,7 @@ std::pair<double, double> convert_grid_numbers_to_coordinates(
     double longitude =
         (grid_longitude_min + grid_longitude_max) / 2;
 
-    return {latitude, longitude};
+    return {longitude, latitude};
 }
 
 std::pair<double, double> geo_decode(uint64_t geo_code) {
@@ -488,8 +488,8 @@ std::pair<double, double> geo_decode(uint64_t geo_code) {
         compact_int64_to_int32(y);
 
     return convert_grid_numbers_to_coordinates(
-        grid_longitude_number,
-        grid_latitude_number
+        grid_latitude_number,
+        grid_longitude_number
     );
 }
 
